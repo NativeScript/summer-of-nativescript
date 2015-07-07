be5b4e6efdca12a9550152fb0154d1331d865881
Clark Sell <clark.sell@csell.net>
2015-07-07 13:49:42 -0500

    databinding goo

--- july/DemoCode/app/Views/DataBinding/DataBinding.js
+++ july/DemoCode/app/Views/DataBinding/DataBinding.js
@@ -1,5 +1,7 @@
 
 var observableModule = require("data/observable");
+var httpModule = require("http");
+
 var _viewModel = new observableModule.Observable();
 
 //My Global
@@ -7,5 +9,48 @@ var _page;
 
 exports.loaded = function(args) {
   _page = args.object;
-  _page.ios.title = "Layout and Controls";
+  _page.ios.title = "DataBinding";
+  _page.bindingContext = _viewModel;
 };
+
+exports.navigatedTo = function(args) {
+  getSessions().then(function(result){
+    var sessions = JSON.parse(result.content);
+    _viewModel.set("sessions", sessions);
+    _viewModel.set("urlConverter", urlConverter);
+
+  });
+};
+
+var urlConverter = {
+  toView: function(url){
+    
+    var headshotUrl = url.get("HeadShot")
+    console.log(headshotUrl);
+    console.log("converter called");
+
+    return 'https://www.thatconference.com' + headshotUrl + '?width=100';
+  }
+}
+/*
+exports.urlConverter = function(url){
+  console.log("converter was called");
+
+  return 'https://www.thatconference.com' + url + '?width=100';
+};
+*/
+
+function getSessions(){
+  // That Conference Sessions
+  var tc_url = "https://www.thatconference.com/api3/Session/GetAcceptedSessions";
+
+  var requestOptions = {
+    method: "GET",
+    url: tc_url,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  return httpModule.request(requestOptions);
+}
